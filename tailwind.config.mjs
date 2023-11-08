3a89a5569baeefd4fadb6c2e008734659bbf24fd
EdamAme-x <edame8080@gmail.com>
2023-11-08 11:58:15 +0900

    add tailwind-css / aspectRatio plugin

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
+import AspectRatio from "@tailwindcss/aspect-ratio";
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		AspectRatio
+	],
 }
