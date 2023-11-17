3dc7793c4fd98dfde68cd94c05785c032cffd4b2
s0u7a <kitan3409@outlook.com>
2023-11-17 22:22:50 +0900

    added offline support

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,59 @@
-self.addEventListener('activate', (event) => {
-    event.waitUntil(self.clients.claim());
-  });
-  self.addEventListener('install', (event) => {
-      console.log('インストール');
-     event.waitUntil(self.skipWaiting());
-  });
-  self.addEventListener("fetch", (e) => {
-    console.log("[Service Worker] Fetched resource " + e.request.url);
-  });
+//キャッシュ名(=バージョン)を指定する
+var CACHE_NAME = "cache-v1";
+//キャッシュするファイル or ディレクトリを指定する
+var urlsToCache = [
+  "/",
+];
+
+// install
+self.addEventListener("install", function (event) {
+  event.waitUntil(self.skipWaiting());
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+// activate
+self.addEventListener("activate", function (event) {
+  var cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+// fetch
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      }
+
+      var fetchRequest = event.request.clone();
+
+      return fetch(fetchRequest).then(function (response) {
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+
+        var responseToCache = response.clone();
+
+        caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
+  );
+});
